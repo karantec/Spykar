415fdfcdb108d983b741f7d0c872c7b54ee4bcab
karantec <karanrana3095@gmail.com>
2025-02-27 00:15:48 +0530

    finalcommit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const createError = require('http-errors');
-
+const UserRoutes=require('./routes/User.route');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 require('dotenv').config();
@@ -10,10 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 connectDB()
+
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
 });
-
+ 
+app.use('/auth', UserRoutes);
 app.use('/api', require('./routes/api.route'));
 
 app.use((req, res, next) => {
