e3ba5752b61798ca6bc1f6a98e891fa060a8b359
karantec <karanrana3095@gmail.com>
2025-03-02 14:22:32 +0530

    chefdone

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const BlogRoutes=require('./routes/Blog.route')
 const TestimonialRoutes=require('./routes/Testimonial.route');
 const GalleryRoutes=require('./routes/Gallery.route');
 const CrouselRoutes=require('./routes/Crousel.route');
+const chefRoutes=require('./routes/Chef.route');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 require('dotenv').config();
@@ -24,6 +25,7 @@ app.use('/blog',  BlogRoutes)
 app.use('/testimonial', TestimonialRoutes );
 app.use('/gallery',GalleryRoutes)
 app.use('/crousel', CrouselRoutes)
+app.use('/chef',chefRoutes);
 app.use('/api', require('./routes/api.route'));
 
 app.use((req, res, next) => {
